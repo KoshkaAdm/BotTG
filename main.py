3396ff19a8df0f2db74f7e3e3f4bff5db124d04d
KoshkaAdm <golicyna88@gmail.com>
2025-07-29 20:42:49 +0500

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,12 @@
 from aiogram import Bot, Dispatcher
 from aiogram.utils import executor
 from config import BOT_TOKEN
-from handlers import dp
+from handlers import register_handlers
 
 bot = Bot(token=BOT_TOKEN)
-dp.bot = bot
+dp = Dispatcher(bot)
 
-if __name__ == "__main__":
+register_handlers(dp)
+
+if name == "main":
     executor.start_polling(dp, skip_updates=True)
